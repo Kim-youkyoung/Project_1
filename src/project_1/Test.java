f6f2a55a50a58e7d97f167c8c02629b3b06c5f96
jsk <whtjdrnr010@naver.com>
2020-10-21 15:02:30 +0900

    test

--- src/project_1/Test.java
+++ src/project_1/Test.java
@@ -3,7 +3,7 @@ package project_1;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 1234s
+		// TODO Auto-generated method stub 1234sfsdffsd
 		ProjectUI pui = new ProjectUI();
 		System.out.println("hi");
 	}
