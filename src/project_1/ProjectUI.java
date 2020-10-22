08c0e0f32eb852c40154357a35bba1072e0fdce5
Kim-youkyoung <yurud822@gmail.com>
2020-10-22 15:32:16 +0900

    rrrr

--- src/project_1/ProjectUI.java
+++ src/project_1/ProjectUI.java
@@ -31,7 +31,6 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 
 public class ProjectUI {
- /////////////////////////////
    //field
    ProjectEvent pro_event;
    
